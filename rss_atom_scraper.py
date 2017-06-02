57abfda628eda4d89f3645dcd670a5c053ee2a71
czimbortibor <czimbortibor@gmail.com>
2017-06-02 15:41:56 +0300

    minor issues fixed

--- rss_atom_scraper.py
+++ rss_atom_scraper.py
@@ -13,13 +13,13 @@ def db_connect(URI, db_name, collection_name):
 
 
 def main(argv=None):
-    if argv is None:
+    if len(argv) == 1:
         feeds_file_input = 'feed_list.json'
     else:
-        feeds_file_input = argv
-    print(feeds_file_input)
+        feeds_file_input = argv[1]
+    print('reading {0} ...'.format(feeds_file_input))
     feed_list, feed_list_jsondata = config_handler.load_feed_list(feeds_file_input)
-    print('feeds: {0}'.format(len(feed_list)))
+    print('feeds: {0}\n'.format(len(feed_list)))
 
     URI = 'mongodb://localhost:27017'
     db_name = 'local'
@@ -33,7 +33,7 @@ def main(argv=None):
     scraper = Scraper(feed_list)
 
     entries = scraper.get_entries()
-    # get the metadata in interest
+    # get the metadata in interest and the images
     with Pool() as pool:
         metadata = pool.map(scraper.get_metadata, entries)
 
@@ -41,12 +41,9 @@ def main(argv=None):
     print('inserting feeds into the database...\n')
     db_collection.insert_many(metadata)
 
-    # get the images
-    with Pool() as pool:
-        img_urls = pool.map(scraper.scrape_images, entries)
-
+    print('\ndownloading the images...\n')
     # download the images and return the directory name
-    img_dir = scraper.download_images(img_urls)
+    img_dir = scraper.download_images(metadata)
 
     print('inserting image collection path into the database...\n')
     db_collection = db_context.change_collection('ImageCollections')
--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 
 import feedparser
 import urllib3
+from bs4 import BeautifulSoup
 
 
 '''
@@ -40,10 +41,13 @@ class Scraper:
 
         if 'title' in entry:
             metadata['title'] = entry['title']
+        if 'author' in entry:
+            metadata['author'] = entry['author']
         if 'title_detail' in entry:
             metadata['rss'] = entry['title_detail']['base']
         if 'summary' in entry:
-            metadata['summary'] = entry['summary']
+            # remove the html tags
+            metadata['summary'] = BeautifulSoup(entry['summary'], 'lxml').text
         if 'published' in entry:
             metadata['published'] = entry['published']
         if 'updated' in entry:
@@ -51,9 +55,11 @@ class Scraper:
         if 'link' in entry:
             metadata['link'] = entry['link']
 
+        metadata['image_url'] = self.scrape_image(entry)
+
         return metadata
 
-    def scrape_images(self, entry):
+    def scrape_image(self, entry):
         pattern_src = re.compile('(src=.*)|(img src=.*)')
         pattern_url = re.compile('url=.*')
         contents = []
@@ -74,6 +80,8 @@ class Scraper:
                     if isinstance(tag, dict):
                         if 'value' in tag:
                             href = pattern_src.search(tag['value'])
+                            if href:
+                                return href.group().split('\"')[1]
                         else:
                             href = tag['url']
                             return href
@@ -93,8 +101,7 @@ class Scraper:
                     if href:
                         return href.group()
 
-    def download_images(self, img_urls):
-        print('\ndownloading the images...\n')
+    def download_images(self, metadata):
         img_dir = str(datetime.date.today())
         if not os.path.exists(img_dir):
             os.makedirs(img_dir)
@@ -102,7 +109,8 @@ class Scraper:
 
         count = 0
         http = urllib3.PoolManager()
-        for url in img_urls:
+        for item in metadata:
+            url = item['image_url']
             try:
                 response = http.request('GET', url)
             except:
